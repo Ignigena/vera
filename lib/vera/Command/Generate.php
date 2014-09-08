a9f8c4d4b4a08666ea259b5cd586aed3008f17d8
Albert Martin <albert@astonishdesign.com>
2014-09-08 12:04:37 -0500

    Implemented ability to override existing install.

--- lib/vera/Command/Generate.php
+++ lib/vera/Command/Generate.php
@@ -14,17 +14,17 @@ class Generate extends \Vera\Command {
     if (!drush_confirm(dt('Generate new site structure in the current working directory?')))
       return;
 
-    if (file_exists('docroot'))
-      return drush_set_error('VERA_GENERATE_EXISTS', 'A Drupal installation already exists.');
+    if (file_exists('docroot') && !drush_confirm(dt('A Drupal installation already exists. Continue?')))
+      return;
 
-    $this->chooseInstallation();
+    $this->chooseInstallation(file_exists('docroot'));
 
     $profile = str_replace('Command/Generate.php', 'Profile/vera', __FILE__);
     $fs = new Filesystem();
-    $fs->mirror($profile, 'docroot/sites/all/modules/custom');
+    $fs->mirror($profile, 'docroot/sites/all/modules/custom/vera');
   }
 
-  function chooseInstallation() {
+  function chooseInstallation($exists) {
     if (!drush_get_option('profile')) {
       $makedir = str_replace('Command/Generate.php', 'Profile/make', __FILE__);
       $makefiles = array_diff(scandir($makedir), array('..', '.'));
@@ -35,12 +35,21 @@ class Generate extends \Vera\Command {
       }
 
       $make = drush_choice($options, 'Choose a Drupal make profile');
-    } else {
+    }
+    else {
       $make = drush_get_option('profile');
     }
 
     $make = str_replace('Command/Generate.php', 'Profile/make/' . $make . '.make', __FILE__);
     drush_log(dt('Downloading Drupal, this will take a moment.'), 'warning');
+
+    if ($exists) {
+      drush_set_option('no-core', TRUE);
+    }
+    else {
+      drush_set_option('prepare-install', TRUE);
+    }
+
     drush_invoke('make', array($make, 'docroot'));
   }
 
---
+++ lib/vera/Profile/make/acquia.make
@@ -0,0 +1,58 @@
+core = 7.x
+api = 2
+
+projects[drupal][version] = "7.31"
+
+projects[ctools][subdir] = "contrib"
+projects[ctools][version] = "1.4"
+
+projects[date][subdir] = "contrib"
+projects[date][version] = "2.8"
+
+projects[entity][subdir] = "contrib"
+projects[entity][version] = "1.5"
+
+projects[entityreference][subdir] = "contrib"
+projects[entityreference][version] = "1.1"
+
+projects[escape_admin][subdir] = "contrib"
+projects[escape_admin][version] = "1.1"
+
+projects[file_entity][subdir] = "contrib"
+projects[file_entity][version] = "2.0-alpha3"
+
+projects[inline_entity_form][subdir] = "contrib"
+projects[inline_entity_form][version] = "1.5"
+
+projects[jquery_update][subdir] = "contrib"
+projects[jquery_update][version] = "2.4"
+
+projects[libraries][subdir] = "contrib"
+projects[libraries][version] = "2.2"
+
+projects[link][subdir] = "contrib"
+projects[link][version] = "1.2"
+
+projects[media][subdir] = "contrib"
+projects[media][version] = "2.0-alpha3"
+
+projects[navbar][subdir] = "contrib"
+projects[navbar][version] = "1.4"
+
+projects[pathauto][subdir] = "contrib"
+projects[pathauto][version] = "1.2"
+
+projects[rules][subdir] = "contrib"
+projects[rules][version] = "2.7"
+
+projects[token][subdir] = "contrib"
+projects[token][version] = "1.5"
+
+projects[variable][subdir] = "contrib"
+projects[variable][version] = "2.5"
+
+projects[views][subdir] = "contrib"
+projects[views][version] = "3.8"
+
+projects[xmlsitemap][subdir] = "contrib"
+projects[xmlsitemap][version] = "2.0"
